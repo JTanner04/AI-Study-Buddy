feff36949d1ebfb9e88532d0dbff13fa10e8f859
JTanner04 <138722231+JTanner04@users.noreply.github.com>
2024-11-25 19:11:54 -0600

    settings.py

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'study_buddy_db', 
+        'USER': 'my_db_user',    
+        'PASSWORD': 'my_db_password',  
+        'HOST': 'localhost',
+        'PORT': '5432', 
+    }
+}
